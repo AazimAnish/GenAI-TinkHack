5b288513ab24e47315dfe860851869fc6ad3ed19
MuhammedYahiya <yahiyahmhd@gmail.com>
2023-11-11 22:38:19 +0530

    feat(api)!: create api for subscription

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const userRouter = require('./routers/userRouter')
 const salaryRouter = require('./routers/salaryRouter')
 const loanRouter = require('./routers/loanRouter')
+const subscriptionRouter = require('./routers/subcriptionRouter')
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 app.use('/users',userRouter)
 app.use('/salary',salaryRouter)
 app.use('/loan',loanRouter)
+app.use('/subscriptions',subscriptionRouter)
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
